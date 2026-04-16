cbc10faec4ee2b21cdeff4c4ceb2c94f9c7ca169
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-04-16 10:36:21 +0100

    Improve crystal model
    This adds a second mesh directly to the savegame crystal model to allow it to be swapped in-game.

--- src/Types/TR1/Crystals/TR1CrystalBuilder.cs
+++ src/Types/TR1/Crystals/TR1CrystalBuilder.cs
@@ -1,7 +1,4 @@
-﻿using TRImageControl;
-using TRLevelControl;
-using TRLevelControl.Helpers;
-using TRLevelControl.Model;
+﻿using TRLevelControl.Model;
 using TRXInjectionTool.Control;
 using TRXInjectionTool.Util;
 
@@ -10,7 +7,7 @@ namespace TRXInjectionTool.Types.TR1.Crystals;
 public class TR1CrystalBuilder : InjectionBuilder
 {
     public override List<InjectionData> Build()
-        => [.. CreatePlacements(), CreatePSXModel()];
+        => [.. CreatePlacements(), CreateModel()];
 
     private static IEnumerable<InjectionData> CreatePlacements()
     {
@@ -25,29 +22,6 @@ public class TR1CrystalBuilder : InjectionBuilder
         });
     }
 
-    private static InjectionData CreatePSXModel()
-    {
-        var level = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
-        var model = level.Models[TR1Type.SavegameCrystal_P];
-        ResetLevel(level);
-        level.Models[TR1Type.SavegameCrystal_P] = model;
-
-        var img = new TRImage(TRConsts.TPageWidth, TRConsts.TPageHeight);
-        img.Fill(CrystalUtils.Purple);
-        level.ObjectTextures.Add(new TRObjectTexture(0, 0, 8, 8));
-
-        model.Meshes.ForEach(m =>
-        {
-            m.TexturedRectangles.AddRange(m.ColouredRectangles);
-            m.TexturedTriangles.AddRange(m.ColouredTriangles);
-            m.ColouredRectangles.Clear();
-            m.ColouredTriangles.Clear();
-            m.TexturedFaces.ToList().ForEach(f => f.Texture = 0);
-        });
-
-        var data = InjectionData.Create(level, InjectionType.PSCrystal, "purple_crystal");
-        data.Images.Add(new() { Pixels = img.ToRGBA() });
-        data.SetMeshOnlyModel((uint)TR1Type.SavegameCrystal_P);
-        return data;
-    }
+    private static InjectionData CreateModel()
+        => CrystalUtils.MakeCrystal(TRGameVersion.TR1);
 }
