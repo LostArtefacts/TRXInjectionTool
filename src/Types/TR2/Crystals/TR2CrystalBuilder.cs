2c427a4c94914682e601f71020f5bf7859dc7126
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-04-16 10:36:21 +0100

    Improve crystal model
    This adds a second mesh directly to the savegame crystal model to allow it to be swapped in-game.

--- src/Types/TR2/Crystals/TR2CrystalBuilder.cs
+++ src/Types/TR2/Crystals/TR2CrystalBuilder.cs
@@ -1,8 +1,4 @@
-﻿using System.Drawing;
-using TRImageControl;
-using TRLevelControl;
-using TRLevelControl.Helpers;
-using TRLevelControl.Model;
+﻿using TRLevelControl.Model;
 using TRXInjectionTool.Control;
 using TRXInjectionTool.Util;
 
@@ -11,7 +7,7 @@ namespace TRXInjectionTool.Types.TR2.Crystals;
 public class TR2CrystalBuilder : InjectionBuilder
 {
     public override List<InjectionData> Build()
-        => [.. CreatePlacements(), CreatePSXModel()];
+        => [.. CreatePlacements(), CreateModel()];
 
     private static IEnumerable<InjectionData> CreatePlacements()
     {
@@ -19,45 +15,13 @@ public class TR2CrystalBuilder : InjectionBuilder
         return crystals.Where(kvp => kvp.Value.Count > 0).Select(kvp =>
         {
             var level = _control2.Read($"Resources/{kvp.Key}");
-            var data = CreateData(CrystalUtils.Blue, InjectionType.General, $"{_tr2NameMap[kvp.Key]}_crystals");
+            var data = InjectionData.Create(TRGameVersion.TR2, InjectionType.General, $"{_tr2NameMap[kvp.Key]}_crystals");
             CreateDefaultTests(data, kvp.Key);
             data.FloorEdits.AddRange(CrystalUtils.ConvertItems(kvp.Value, r => level.Rooms[r].Info));
             return data;
         });
     }
 
-    private static InjectionData CreatePSXModel()
-        => CreateData(CrystalUtils.Purple, InjectionType.PSCrystal, "purple_crystal");
-
-    private static InjectionData CreateData(Color color, InjectionType type, string name)
-    {
-        var level = _control2.Read($"Resources/{TR2LevelNames.GW}");
-        ResetLevel(level, 1);
-
-        var img = new TRImage(TRConsts.TPageWidth, TRConsts.TPageHeight);
-        img.Fill(color);
-        level.ObjectTextures.Add(new TRObjectTexture(0, 0, 8, 8));
-
-        var caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
-        var model = caves.Models[TR1Type.SavegameCrystal_P];
-        model.Meshes.ForEach(m =>
-        {
-            m.TexturedRectangles.AddRange(m.ColouredRectangles);
-            m.TexturedTriangles.AddRange(m.ColouredTriangles);
-            m.ColouredRectangles.Clear();
-            m.ColouredTriangles.Clear();
-            m.TexturedFaces.ToList().ForEach(f => f.Texture = 0);
-        });
-
-        level.Models[TR2Type.SavegameCrystal_P] = model;
-        level.Images16[0].Pixels = img.ToRGB555();
-        level.Images8[0].Pixels = img.ToRGB(level.Palette);
-
-        var data = InjectionData.Create(level, type, name);
-        if (type == InjectionType.PSCrystal)
-        {
-            data.SetMeshOnlyModel((uint)TR2Type.SavegameCrystal_P);
-        }
-        return data;
-    }
+    private static InjectionData CreateModel()
+        => CrystalUtils.MakeCrystal(TRGameVersion.TR2);
 }
--- src/Util/CrystalUtils.cs
+++ src/Util/CrystalUtils.cs
@@ -1,14 +1,18 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
+using TRImageControl;
 using TRLevelControl;
+using TRLevelControl.Helpers;
 using TRLevelControl.Model;
 using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
 
 namespace TRXInjectionTool.Util;
 
 public static class CrystalUtils
 {
-    public static readonly Color Blue = Color.FromArgb(188, 220, 220);
-    public static readonly Color Purple = Color.FromArgb(64, 64, 252);
+    private static readonly Color _blue = Color.FromArgb(188, 220, 220);
+    private static readonly Color _purple = Color.FromArgb(64, 64, 252);
 
     public static Dictionary<string, List<TR1Entity>> GetLocations<T>(string path, T type)
     {
@@ -45,5 +49,56 @@ public static class CrystalUtils
                 Item = item,
             }],
         });
+    }
+
+    public static InjectionData MakeCrystal(TRGameVersion version)
+    {
+        var caves = new TR1LevelControl().Read($"Resources/{TR1LevelNames.CAVES}");
+        var model = caves.Models[TR1Type.SavegameCrystal_P];
+        model.Meshes.ForEach(m =>
+        {
+            m.TexturedRectangles.AddRange(m.ColouredRectangles);
+            m.TexturedTriangles.AddRange(m.ColouredTriangles);
+            m.ColouredRectangles.Clear();
+            m.ColouredTriangles.Clear();
+            m.TexturedFaces.ToList().ForEach(f => f.Texture = 0);
+        });
+
+        model.Meshes.Add(model.Meshes[0].Clone());
+        model.MeshTrees.Add(new());
+        model.Meshes[1].TexturedFaces.ToList().ForEach(f => f.Texture = 1);
+        
+        foreach (var fr in model.Animations[0].Frames)
+        {
+            fr.Rotations.Add(new());
+        }
+
+        TRLevelBase level = version switch
+        {
+            TRGameVersion.TR1 => new TR1LevelControl().Read($"Resources/{TR1LevelNames.CAVES}"),
+            TRGameVersion.TR2 => new TR2LevelControl().Read($"Resources/{TR2LevelNames.GW}"),
+            _ => throw new NotSupportedException(),
+        };
+
+        if (level is TR1Level level1)
+        {
+            InjectionBuilder.ResetLevel(level1);
+            level1.Models[TR1Type.SavegameCrystal_P] = model;
+        }
+        else if (level is TR2Level level2)
+        {
+            InjectionBuilder.ResetLevel(level2);
+            level2.Models[TR2Type.SavegameCrystal_P] = model;
+        }
+
+        var img = new TRImage(TRConsts.TPageWidth, TRConsts.TPageHeight);
+        img.Fill(new(0, 0, 8, 8), _blue);
+        img.Fill(new(8, 0, 8, 8), _purple);
+        level.ObjectTextures.Add(new TRObjectTexture(0, 0, 8, 8));
+        level.ObjectTextures.Add(new TRObjectTexture(8, 0, 8, 8));
+
+        var data = InjectionData.Create(level, InjectionType.General, "crystal");
+        data.Images.Add(new() { Pixels = img.ToRGBA() });
+        return data;
     }
 }
